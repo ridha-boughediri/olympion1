efb9d2e1a9b29c4435c8e1bdee16ee8d2e51ee87
ridha-boughediri <ridha.boughediri@laplateforme.io>
2022-11-15 15:34:06 +0100

    update

---
+++ frontolympion/src/Pages/ServiceTransport/ServiceTransport.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function ServiceTransport() {
+  
+
+
+    //auth
+    //user id 
+    ///call api add wallet 
+    //api add metric 
+
+  
+
+  return (
+    <div>
+      wwaiting
+    </div>
+  );
+}
+
+export default ServiceTransport;
