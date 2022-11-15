4b56fb853ab12580beca4fc92f478f458ba5d580
selimamri <113339965+selimamri@users.noreply.github.com>
2022-11-15 17:32:21 +0100

    Delete backolympion directory

--- backolympion/src/api/itineraire/services/itineraire.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * itineraire service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::itineraire.itineraire');
--- backolympion/src/api/metrique/services/metrique.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * metrique service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::metrique.metrique');
