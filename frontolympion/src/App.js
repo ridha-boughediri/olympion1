6855424370851a8b8e3e2d2e31c5db30f6318111
ridha-boughediri <ridha.boughediri@laplateforme.io>
2022-11-16 10:28:30 +0100

    delete link servicetransport

--- frontolympion/src/App.js
+++ frontolympion/src/App.js
@@ -3,7 +3,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Pages/Home";
 import Presents from "./Pages/Presents";
-import ServiceTransport from "./Pages/ServiceTransport/ServiceTransport";
 import Account from "./Pages/Account";
 import Navig from "./Components/Navig";
 import Footer from "./Components/Footer";
@@ -15,11 +14,7 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Home />}></Route>
           <Route path="/presents" exact element={<Presents />}></Route>
-          <Route
-            path="/ServiceTranport/services/transport"
-            exact
-            element={<ServiceTransport />}
-          ></Route>
+         
           <Route path="/account" exact element={<Account />}></Route>
         </Routes>
         <Footer />
