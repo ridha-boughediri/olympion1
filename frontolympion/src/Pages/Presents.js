48f1791ae61e41894d85e6ab34d8735382b1bd9e
amris <selim.amri@epitech.eu>
2022-11-16 10:11:28 +0100

    correction cadeaux

--- frontolympion/src/Pages/Presents.js
+++ frontolympion/src/Pages/Presents.js
@@ -7,63 +7,63 @@ import "../Css/Presents.css";
 function Presents() {
   const clarisse = {
     type: "Clarisse Agbegnenou",
-    descritpion: "Uniforme de judo : 100 Oly",
+    descritpion: "Uniforme de judo : 500 Olympions",
     img: require("../Css/Images/Photo_athlètes/clarisse_agbegnenou_judo.jpg"),
   };
   const kevin = {
     type: "Kevin Mayer",
-    descritpion: "Maillot de décathlon : 100 Oly",
+    descritpion: "Maillot de décathlon : 300 Olympions",
     img: require("../Css/Images/Photo_athlètes/kevin_mayer_decathlon.jpg"),
   };
 
   const charline = {
     type: "Charline Picon",
-    descritpion: "Maillot de voile : 100 Oly",
+    descritpion: "Maillot de voile : 300 Olympions",
     img: require("../Css/Images/Photo_athlètes/Charline_Picon_voile.jpg"),
   };
   const nicolas = {
     type: "Nicolas Batum",
-    descritpion: "Chasuble de basket : 100 Oly",
+    descritpion: "Chasuble de basket : 250 Olympions",
     img: require("../Css/Images/Photo_athlètes/nicolas_batum_basket.jpg"),
   };
   const florent = {
     type: "Florent Manodou",
-    descritpion: "Slip de bain : 100 Oly",
+    descritpion: "Slip de bain : 300 Olympions",
     img: require("../Css/Images/Photo_athlètes/florent_manodou_natation.jpg"),
   };
   const teddy = {
     type: "Teddy Riner",
-    descritpion: "Uniforme de judo : 100 Oly",
+    descritpion: "Uniforme de judo : 500 Olympions",
     img: require("../Css/Images/Photo_athlètes/teddy_riner_judo.jpg"),
   };
   const julian = {
     type: "Julian Alaphilippe",
-    descritpion: "Maillot de cyclisme : 100 Oly",
+    descritpion: "Maillot de cyclisme : 800 Olympions",
     img: require("../Css/Images/Photo_athlètes/julian_alaphilippe_velo.jpg"),
   };
   const analia = {
     type: "Analia Pigree",
-    descritpion: "Body de piscine: 100 Oly",
+    descritpion: "Body de piscine: 300 Olympions",
     img: require("../Css/Images/Photo_athlètes/analia_pigree_natation.jpg"),
   };
   const eunice = {
     type: "Eunice Barber",
-    descritpion: "Maillot de heptatlon: 100 Oly",
+    descritpion: "Maillot de heptatlon: 300 Olympions",
     img: require("../Css/Images/Photo_athlètes/eunice_barber_heptatlon.jpg"),
   };
   const juliette = {
     type: "Juliette Labous",
-    descritpion: "Maillot de cyclisme: 100 Oly",
+    descritpion: "Maillot de cyclisme: 800 Olympions",
     img: require("../Css/Images/Photo_athlètes/juliette_labous_velo.jpg"),
   };
   const marine = {
     type: "Marine Johannes",
-    descritpion: "Chasuble de basket: 100 Oly",
+    descritpion: "Chasuble de basket: 250 Olympions",
     img: require("../Css/Images/Photo_athlètes/marine_johannes_basket.jpg"),
   };
   const thomas = {
     type: "Thomas Goyard",
-    descritpion: "Maillot de voile: 100 Oly",
+    descritpion: "Maillot de voile: 300 Olympions",
     img: require("../Css/Images/Photo_athlètes/thomas_goyard_voile.jpg"),
   };
 
